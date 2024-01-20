36b1bc52fe71e0ce90dcc65022569621a3b593de
viv2005ek <142710112+viv2005ek@users.noreply.github.com>
2024-01-20 09:16:46 +0000

    Updating Web Page

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 
 
+let mode = "light";
+
+let mod = document.querySelector("#mode");
+document.body.style.backgroundImage="url(whiteDesginBack.jpg)";
+
+mod.addEventListener("click", (e) => {
+  if (mode == "light") {
+    mode = "dark";
+document.body.style.backgroundImage="url(blackDesginBack.jpg)";
+  } else {
+    mode = "light";
+document.body.style.backgroundImage="url(vinisha_delete.jpg)";
+
+  }
+});
+
 function goToBookmark(value) {
     window.location.href = value;
   }
 
-let mode="light";
+
+let gchild1=document.querySelector("gchild1");
+let gchild3=document.querySelector("gchild3");
+let gchild5=document.querySelector("gchild5");
+
+gchild1.addEventLisetener("click",()=>{
+window.location.href = "";
+})
+
+child3.addEventLisetener("click",()=>{
+  goToBookmark("https://www.google.com/");
+})
+child5.addEventLisetener("click",()=>{
+  goToBookmark("https://www.google.com/");
+})
 
 
-let mod=document.querySelector("#mode");
-mod.addEventListener("click",(e) =>{
-  if (mode === "light") {
-    mode = "dark";
-    document.body.style.backgroundColor = "black";
-  } else {
-    mode = "light";
-     document.body.style.backgroundColor = "white";
-  }}
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
